e92c65afb1023bafdbfe3cc581ac1b12cb168458
Sporego <contact@filbert.io>
2020-09-05 15:09:04 -0400

    Base feature set complete

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,62 @@
 	<title>Boardwalk Kennel</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="container">
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+	<div class="row">
+		<div class="col-md">
+			<div class="card border-info mt-4 mb-4">
+				<div class="card-header">Info</div>
+				<div class="card-body">
+				  	<h4 class="card-title">Work in process!</h4>
+					<p class="card-text">Building for L.E.'s Boardwalk Kennel</p>
+				  		<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100" style="width: 33%"></div>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+	<div class="row">
+		<div class="col-md">
+			<div class="card mb-3">
+				<div class="card-body">
+				  <h5 class="card-title">Special title treatment</h5>
+				  <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+				</div>
+				<img src="happysamoyed.png" class="img-fluid">
+				<div class="card-body">
+				  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+				<div class="card-footer text-muted">
+				  2 days ago
+				</div>
+			  </div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md">
+			<blockquote class="blockquote">
+				<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+				<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+			</blockquote>
+		</div>
+		<div class="col-md">
+			<blockquote class="blockquote text-center">
+				<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+				<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+			</blockquote>
+		</div>
+		<div class="col-md">
+			<blockquote class="blockquote text-right">
+				<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+				<footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+			</blockquote>
+		</div>
+	</div>	
+</div>
+
+
+
